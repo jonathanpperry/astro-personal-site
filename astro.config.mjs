a3f84f2cb512af1f336035b395ef674dafdf7b25
Jonathan Perry <jpper@umich.edu>
2023-11-01 15:32:14 -0500

    Added vercel integration package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
